d1ce7fec08932ac21ce42d27e38d13aa6c42cc11
Karuppusamy <d.karuppusamy@outlook.com>
2021-04-27 02:10:11 +0530

    updated projects page

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,27 @@
 const projectsData = [
   {
-    title: "A Search Engine",
-    description: `What is you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: "/static/images/google.png",
-    href: "https://www.google.com",
+    title: "Meet Attendance",
+    description: `This is a Chrome Extension which is used to take attendance during online classes/Meetings in Google Meet. It saves the Attendance log as a HTML file.`,
+    imgSrc: "/static/images/projects/meet-attendance.png",
+    href: "https://github.com/Karuppusamy-D/meet-attendance",
   },
   {
-    title: "The Time Machine",
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: "/static/images/time-machine.jpg",
-    href: "/blog/the-time-machine",
+    title: "HiFi Stereo Amplifier Using TDA7292 IC",
+    description: `I designed a HiFi Stereo Amplifier based of TDA7292 IC, which can provide up-to 40W+40W power output. It is a 10cm*10cm PCB. Mostly I used Metal Film, Mica and Ceramic Capacitors, So It can deliver High Quality sound. I have also Isolated High Power Components with Low Power Components to reduce noise. It also has a Mute and Standby circuits for effectively reduce power when not in use.`,
+    imgSrc: "/static/images/projects/amplifier.png",
+    href: "https://github.com/Karuppusamy-D/TDA7292-Amplifier/",
+  },
+  {
+    title: "Water Level Monitor & Controller",
+    description: `It is based on ESP32 / NodeMCU and contains Ultrasonic Sensor and a Relay. It automatically controls water pump and fills the tanks when empty. It can be used in houses or elevated reservoirs. It integrates with Google Assistant and Specially designed Android App, So we can monitor and control water tanks through Mobile app Website or Google Assistant anytime anywhere. It's cost is very low because of fewer components.`,
+    imgSrc: "/static/images/projects/project.png",
+    // href: "/blog/water-level-controller",
+  },
+  {
+    title: "Multimeter Based on Arduino",
+    description: `It is based on Arduino Nano and it can measure Voltage, Current, Resistance, Continuity, Capacitance and Inductance with petty good accuracy and speed. It can handle current of 10A and voltage ranges from 0V to 50V with 6MOhm I/P Impedance. It's output is displayed in a 16x2 LED Display.`,
+    imgSrc: "/static/images/projects/project.png",
+    // href: "/blog/multimeter",
   },
 ];
 
