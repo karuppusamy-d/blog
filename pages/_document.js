8da1c63f446b3f5d0fa01f457846675f7c3b0128
Karuppusamy <d.karuppusamy@outlook.com>
2021-04-28 19:01:28 +0530

    added fontweight Poppins@500

--- pages/_document.js
+++ pages/_document.js
@@ -46,11 +46,11 @@ class MyDocument extends Document {
           <link
             rel="preload"
             as="style"
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           />
 
           <link
