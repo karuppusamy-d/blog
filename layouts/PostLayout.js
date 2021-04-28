93ae7b4a81f7442c0165c4c3c82fb29effabb7f6
Karuppusamy <d.karuppusamy@outlook.com>
2021-04-28 19:50:44 +0530

    updated PostLayout styles

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -29,12 +29,12 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
       />
       <article>
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-          <header className="pt-6 xl:pb-6">
+          <header className="pt-8 xl:pb-10 xl:pt-12">
             <div className="space-y-1 text-center">
               <dl className="space-y-10">
                 <div>
                   <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                  <dd className="text-base font-medium leading-8 text-gray-500 dark:text-gray-400">
                     <time dateTime={date}>
                       {new Date(date).toLocaleDateString(
                         siteMetadata.locale,
@@ -49,11 +49,12 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
               </div>
             </div>
           </header>
+
           <div
-            className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6"
+            className="divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6"
             style={{ gridTemplateRows: "auto 1fr" }}
           >
-            <dl className="pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200 xl:dark:border-gray-700">
+            <dl className="pt-6 pb-8 xl:py-8 xl:border-b xl:border-gray-200 xl:dark:border-gray-700">
               <dt className="sr-only">Authors</dt>
               <dd>
                 <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
@@ -65,11 +66,11 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
                     />
                     <dl className="text-sm font-semibold whitespace-nowrap">
                       <dt className="sr-only">Name</dt>
-                      <dd className="text-gray-900 dark:text-gray-200">
+                      <dd className="text-gray-800 dark:text-gray-300">
                         {siteMetadata.author}
                       </dd>
                       <dt className="sr-only">Headline</dt>
-                      <dd className="text-xs text-gray-500 dark:text-gray-400">
+                      <dd className="text-xs font-medium text-gray-500 dark:text-gray-400">
                         {siteMetadata.headline}
                       </dd>
                     </dl>
@@ -78,7 +79,7 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
               </dd>
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
-              <div className="pt-10 pb-8 prose dark:prose-dark max-w-none">
+              <div className="pt-8 pb-6 prose dark:prose-dark max-w-none">
                 {children}
               </div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
@@ -89,10 +90,10 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
                 <Link href={editUrl(fileName)}>{"View on GitHub"}</Link>
               </div>
             </div>
-            <footer>
+            <div>
               <div className="text-sm font-medium leading-5 divide-gray-200 xl:divide-y dark:divide-gray-700 xl:col-start-1 xl:row-start-2">
                 {tags && (
-                  <div className="py-4 xl:py-8">
+                  <div className="pt-8 pb-4 xl:py-8">
                     <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
                       Tags
                     </h2>
@@ -104,7 +105,7 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
                   </div>
                 )}
                 {(next || prev) && (
-                  <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
+                  <div className="flex justify-between pt-4 pb-2 xl:block xl:space-y-8 xl:py-8">
                     {prev && (
                       <div>
                         <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
@@ -128,15 +129,15 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
                   </div>
                 )}
               </div>
-              <div className="pt-4 xl:pt-8">
+              {/* <div className="pt-4 xl:pt-8">
                 <Link
                   href="/blog"
                   className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
                 >
                   &larr; Back to the blog
                 </Link>
-              </div>
-            </footer>
+              </div> */}
+            </div>
           </div>
         </div>
       </article>
