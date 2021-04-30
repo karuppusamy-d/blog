d5c585b8091c88bc4e32225b2a66670659d3df7d
Karuppusamy <d.karuppusamy@outlook.com>
2021-04-30 18:29:44 +0530

    Revert "improved google font loading speed"
    Reason: Next js 10.2 Do Automatic Webfont Optimization
    https://nextjs.org/blog/next-10-2#automatic-webfont-optimization
    
    This reverts commit ee5c0607807181cb8a8c973b0598d6d17c9dfcaa.

--- pages/_app.js
+++ pages/_app.js
@@ -15,18 +15,6 @@ export default function App({ Component, pageProps }) {
       <MDXProvider components={MDXComponents}>
         <Head>
           <meta content="width=device-width, initial-scale=1" name="viewport" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            media="print"
-            onLoad="this.media='all'"
-          />
-          <noscript>
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            />
-          </noscript>
         </Head>
         <DefaultSeo {...SEO} />
         <LayoutWrapper>
--- pages/_document.js
+++ pages/_document.js
@@ -48,6 +48,10 @@ class MyDocument extends Document {
             as="style"
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+          />
 
           <link
             rel="preload"
