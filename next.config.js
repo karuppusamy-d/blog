59d68a78b86d85bf25498d24941c7458c58d39ae
Karuppusamy <d.karuppusamy@outlook.com>
2021-05-02 20:05:07 +0530

    added pwa support

--- next.config.js
+++ next.config.js
@@ -1,41 +1,51 @@
+const withPWA = require("next-pwa");
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-  pageExtensions: ["js", "jsx", "md", "mdx"],
-  future: {
-    webpack5: true,
-  },
-  webpack: (config, { dev, isServer }) => {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|mp4)$/i,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            publicPath: "/_next",
-            name: "static/media/[name].[hash].[ext]",
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    pageExtensions: ["js", "jsx", "md", "mdx"],
+    future: {
+      webpack5: true,
+    },
+    webpack: (config, { dev, isServer }) => {
+      config.module.rules.push({
+        test: /\.(png|jpe?g|gif|mp4)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              publicPath: "/_next",
+              name: "static/media/[name].[hash].[ext]",
+            },
           },
-        },
-      ],
-    });
-
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
+        ],
       });
-    }
 
-    return config;
-  },
-});
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+
+      if (!dev && !isServer) {
+        // Replace React with Preact only in client production build
+        Object.assign(config.resolve.alias, {
+          react: "preact/compat",
+          "react-dom/test-utils": "preact/test-utils",
+          "react-dom": "preact/compat",
+        });
+      }
+
+      return config;
+    },
+
+    pwa: {
+      disable: !isProd,
+    },
+  })
+);
