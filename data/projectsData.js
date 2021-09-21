279a82ff804eb245d3c8b8017e0ea4e7e0c42327
Karuppusamy <d.karuppusamy@outlook.com>
2021-09-21 23:41:14 +0530

    Add new project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,12 @@
 const projectsData = [
   {
+    title: "TechBuz.net",
+    description:
+      "It is a mobile comparison website build on top of Next.Js. I have optimized it for speed and SEO. It also supports PWA so it can be installed as a regular app. It consists of more than 2.6K mobiles and 3.3M pages.",
+    imgSrc: "/static/images/projects/techbuz.png",
+    href: "https://techbuz.net/",
+  },
+  {
     title: "Meet Attendance",
     description:
       "It is a Chrome / Edge extension that logs attendance during online meetings in Google Meet. It monitors all participants between the set interval and outputs data as an HTML file. It also has keyboard shortcuts to toggle the mic and camera.",
@@ -14,18 +21,18 @@ const projectsData = [
     href: "https://github.com/Karuppusamy-D/TDA7292-Amplifier/",
   },
   {
+    title: "Arduino Based Multimeter",
+    description:
+      "It is based on Arduino Nano. It can measure Voltage, Current, Resistance, Continuity, Capacitance and Inductance with good accuracy and speed. It can handle the current of 10A and voltage ranges from 0V to 50V with 6MOhm I/P Impedance. Its output is displayed in a 16x2 LED Display.",
+    imgSrc: "/static/images/projects/multimeter.svg",
+    // href: "/blog/multimeter",
+  },
+  {
     title: "Water Level Controller",
     description:
       "It is based on ESP32 / ESP8266 and contains only an ultrasonic sensor and a relay. It automatically controls the water pump and fills the tanks when empty. It integrates with google assistant and a specially designed android app. Its cost is low because of fewer components.",
     imgSrc: "/static/images/twitter-card.png",
     // href: "/blog/water-level-controller",
-  },
-  {
-    title: "Arduino Based Multimeter",
-    description:
-      "It is based on Arduino Nano. It can measure Voltage, Current, Resistance, Continuity, Capacitance and Inductance with good accuracy and speed. It can handle the current of 10A and voltage ranges from 0V to 50V with 6MOhm I/P Impedance. Its output is displayed in a 16x2 LED Display.",
-    imgSrc: "/static/images/twitter-card.png",
-    // href: "/blog/multimeter",
   },
 ];
 
