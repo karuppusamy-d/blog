a7348bcaa679b3cdc2813d912cf59cce594fa9fc
Karuppusamy <d.karuppusamy@outlook.com>
2021-11-29 23:40:54 +0530

    Add new project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,22 +1,28 @@
 const projectsData = [
   {
+    title: "io.karuppusamy.me",
+    description: "comming soon...",
+    imgSrc: "/static/images/twitter-card.png",
+    href: "https://io.karuppusamy.me/",
+  },
+  {
     title: "TechBuz.net",
     description:
-      "It is a mobile comparison website build on top of Next.Js. I have optimized it for speed and SEO. It also supports PWA so it can be installed as a regular app. It consists of more than 2.6K mobiles and 3.3M pages.",
+      "It is a mobile comparison website built on top of Next.Js. I have optimized it for speed and SEO. It also supports PWA so it can be installed as a regular app. It consists of more than 2.6K mobiles and 3.3M pages.",
     imgSrc: "/static/images/projects/techbuz.png",
     href: "https://techbuz.net/",
   },
   {
     title: "Meet Attendance",
     description:
-      "It is a Chrome / Edge extension that logs attendance during online meetings in Google Meet. It monitors all participants between the set interval and outputs data as an HTML file. It also has keyboard shortcuts to toggle the mic and camera.",
+      "It is a Chrome / Edge extension that logs attendance during online meetings in Google Meet. It monitors all participants in a set interval and outputs attendance data as an HTML file. It also has keyboard shortcuts to toggle the mic and camera.",
     imgSrc: "/static/images/projects/meet-attendance.png",
     href: "https://github.com/Karuppusamy-D/meet-attendance",
   },
   {
     title: "TDA7292 Amplifier",
     description:
-      "It is a HiFi Stereo Amplifier based on TDA7292 IC, which can provide up to 40W+40W power output. It has isolated High power components and low power components to reduce noise. It also has mute and standby circuits to reduce power when not in use.",
+      "It is a HiFi Stereo Amplifier based on TDA7292 IC, It can provide up to 40W+40W of output power. It has isolated High power components and low power components to reduce noise. It also has mute and standby circuits to reduce power when not in use.",
     imgSrc: "/static/images/projects/amplifier.png",
     href: "https://github.com/Karuppusamy-D/TDA7292-Amplifier/",
   },
