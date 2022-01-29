57531a0db4295b48f9ded22647d4d806e6d8fd9e
Karuppusamy <d.karuppusamy@outlook.com>
2022-01-30 01:22:49 +0530

    Restructured files and folders
    Move components, css, layouts, lib, pages, scripts folders to src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./layouts/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
