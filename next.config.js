1fb483c0e44be841704005fd62d27f54ef60ebe8
Karuppusamy <d.karuppusamy@outlook.com>
2022-01-31 18:05:43 +0530

    Fix preact jsx-runtime error

--- next.config.js
+++ next.config.js
@@ -23,8 +23,8 @@ module.exports = withBundleAnalyzer(
       // Replace React with Preact only in client production build
       if (!dev && !isServer) {
         Object.assign(config.resolve.alias, {
+          "react/jsx-runtime.js": "preact/compat/jsx-runtime",
           react: "preact/compat",
-          "react-dom/test-utils": "preact/test-utils",
           "react-dom": "preact/compat",
         });
       }
